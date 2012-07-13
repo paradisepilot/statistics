5b6cfcaac435f86e0348b5a95084bd1d2fabf81b
Kenneth Chu <paradisepilot@gmail.com>
2012-07-12 22:17:44 -0400

    Updated <github>/paradisepilot/statistics/mixed_effects_models/pinheiro_bates/chap01/exercises/code/exercise-1-2.R

--- mixed_effects_models/pinheiro_bates/chap01/exercises/code/exercise-1-2.R
+++ mixed_effects_models/pinheiro_bates/chap01/exercises/code/exercise-1-2.R
@@ -5,13 +5,29 @@
 
 ### NOTES ##########################################################################################
 
+library(lattice);
 library(nlme);
 data(Oxboys);
 
-##### Chapter 1, Exercise 2 ########################################################################
+str(Oxboys);
+Oxboys;
 
-## (a)
-pdf("../output/exercise-1-1_plot_Oxboys.pdf");
+##### Chapter 1, Exercise 2 ########################################################################
+### (a)
+pdf("../output/exercise-1-2a_plot_Oxboys.pdf");
 plot(Oxboys);
 dev.off();
 
+
+### (b)
+pdf("../output/exercise-1-2b_bwplot_residuals_lm.pdf");
+lm.model <- lm(formula = height ~ age, data = Oxboys);
+bwplot(Subject ~ resid(lm.model), data = Oxboys);
+dev.off();
+
+
+### (c)
+oxboys.lmList <- lmList(height ~ age, data = Oxboys);
+pdf("../output/exercise-1-2c_Oxboys_lmList.pdf");
+plot(oxboys.lmList,Subject ~ resid(.));
+dev.off();
