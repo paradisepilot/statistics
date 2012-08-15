092e0463017619811a77f1355ef6841ddfa47040
Kenneth Chu <paradisepilot@gmail.com>
2012-08-14 21:48:39 -0400

    1

--- generalized_additive_models/wood/chap01/exercises/RUN_R.sh
+++ generalized_additive_models/wood/chap01/exercises/RUN_R.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-# R --no-save < exercise-1-9-1.R > out.R.exercise-1-9-1
-R --no-save < exercise-1-9-8.R > out.R.exercise-1-9-8
+R --no-save < exercise-1-9-01.R > out.R.exercise-1-9-01
+R --no-save < exercise-1-9-08.R > out.R.exercise-1-9-08
 
