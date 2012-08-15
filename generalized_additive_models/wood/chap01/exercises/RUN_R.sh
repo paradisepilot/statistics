e4b0ddb4bf38fe5fae0aa0732a4704b35cecfcea
Kenneth Chu <paradisepilot@gmail.com>
2012-08-14 22:21:50 -0400

    2

--- generalized_additive_models/wood/chap01/exercises/RUN_R.sh
+++ generalized_additive_models/wood/chap01/exercises/RUN_R.sh
@@ -2,4 +2,5 @@
 
 R --no-save < exercise-1-9-01.R > out.R.exercise-1-9-01
 R --no-save < exercise-1-9-08.R > out.R.exercise-1-9-08
+R --no-save < exercise-1-9-09.R > out.R.exercise-1-9-09
 
---
+++ generalized_additive_models/wood/chap01/exercises/exercise-1-9-09.R
@@ -0,0 +1,53 @@
+
+data(warpbreaks);
+str(warpbreaks);
+summary(warpbreaks);
+
+
+### ~~~~~~~~~~~~~~~~~~~~ ###
+LM.warpbreaks.00 <- lm(
+	formula = breaks ~ wool : tension,
+	data    = warpbreaks
+	);
+summary(LM.warpbreaks.00);
+
+
+### ~~~~~~~~~~~~~~~~~~~~ ###
+LM.warpbreaks.01 <- lm(
+	formula = breaks ~ wool * tension,
+	data    = warpbreaks
+	);
+summary(LM.warpbreaks.01);
+
+
+### ~~~~~~~~~~~~~~~~~~~~ ###
+LM.warpbreaks.02 <- lm(
+	formula = breaks ~ tension,
+	data    = warpbreaks
+	);
+summary(LM.warpbreaks.02);
+
+
+### ~~~~~~~~~~~~~~~~~~~~ ###
+anova(LM.warpbreaks.01,LM.warpbreaks.02);
+
+	# The above p-value of 0.01208 means that the difference in goodness-of-fit between the
+	# tension-only model (LM.warpbreaks.02) and the wool+tension+interaction model
+	# (LM.warpbreaks.01) is statistically significant.
+
+### ~~~~~~~~~~~~~~~~~~~~ ###
+pdf("warpbreaks-interaction-plot.pdf");
+
+with(
+	warpbreaks,
+	{
+		interaction.plot(
+			response     = breaks,
+			x.factor     = wool,
+			trace.factor = tension
+			);
+	}
+	);
+
+dev.off();
+
