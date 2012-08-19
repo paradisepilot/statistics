2ef6c914fbd0dbe91b59da90065fde76e4b0b105
Kenneth Chu <paradisepilot@gmail.com>
2012-08-19 19:03:51 -0400

    Added paradisepilot/statistics/general_linear_models/bingham_fry/chap02/exercises/exercise-2-3.R

---
+++ general_linear_models/bingham_fry/chap02/exercises/RUN_R.sh
@@ -0,0 +1,3 @@
+
+R --no-save < exercise-2-3.R 2>&1 > out.R.exercise-2-3
+
---
+++ general_linear_models/bingham_fry/chap02/exercises/exercise-2-3.R
@@ -0,0 +1,8 @@
+
+DF.doughnut <- data.frame(
+	fat.absorbed = c(164,172,168,177,156,178,191,197,182,185,175,193,178,171,163,176,155,166,149,164,170,168),
+	fat.type     = c(rep("Fat1",6),rep("Fat2",6),rep("Fat3",6),rep("Fat4",6))
+	);
+str(DF.doughnut);
+DF.doughnut
+
