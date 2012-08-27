c1b2de478dec7fa355554d541320d20f143fc3e9
Kenneth Chu <paradisepilot@gmail.com>
2012-08-26 23:56:43 -0400

    Added generalized-linear-models

---
+++ generalized-linear-models/dobson-barnett/downloads/RUN_wget.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+wget http://www.crcpress.com/product_resource/C9500/GLM_data.zip
+wget http://www.crcpress.com/product_resource/C9500/C9500_Solutions.pdf
+wget http://www.crcpress.com/product_resource/C9500/GLM_Errata.docx
+
+unzip GLM_data.zip
+
