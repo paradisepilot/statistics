858d19d49df8778fdb61c91e83ab0c8624d6ded8
Kenneth Chu <paradisepilot@gmail.com>
2012-08-27 10:14:36 -0400

    Updated example-9-2-1.R

--- generalized-linear-models/dobson-barnett/chap09/examples/example-9-2-1.R
+++ generalized-linear-models/dobson-barnett/chap09/examples/example-9-2-1.R
@@ -1,2 +1,9 @@
 
-x <- 2;
+FILE.british.doctors <- "../../data/table-09-1_British-doctors-smoking-and-coronary-death.csv";
+
+DF.british.doctors <- read.table(
+	file = FILE.british.doctors,
+	sep = "\t",
+	);
+DF.british.doctors;
+
