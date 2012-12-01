cd564c6f32f6d8106adc622b85cc8ae927b6c4df
paradisepilot <paradisepilot@gmail.com>
2012-12-01 00:50:26 -0500

    Added computational-statistics

---
+++ computational-statistics/givens-hoeting/downloads/RUN_wget.sh
@@ -0,0 +1,7 @@
+
+# wget http://www.stat.colostate.edu/computationalstatistics/datasets/computationalstatistics.zip
+# unzip computationalstatistics.zip
+
+wget http://www.stat.colostate.edu/computationalstatistics/CompStatCode.zip
+unzip CompStatCode.zip
+
