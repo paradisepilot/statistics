7e1a3a972341e8b3f71244f37c7c8254340854d1
Kenneth Chu <paradisepilot@gmail.com>
2013-02-25 22:42:45 -0500

    added: paradisepilot/statistics/design-of-experiments/

---
+++ design-of-experiments/dean-voss/downloads/data/RUN_wget.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+#wget -r -l1 -A.txt http://www.wright.edu/~dvoss/bookdata/txt/aerosol.txt
+
+wget http://www.wright.edu/~dvoss/bookdata/txt/aerosol.txt
+wget http://www.wright.edu/~dvoss/bookdata/txt/heartlung.pump.txt
+
