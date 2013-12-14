19fe84b674cf277e09b5c5567cbb7c0a161f1cd7
paradisepilot <paradisepilot@gmail.com>
2013-12-14 15:04:06 -0500

    updated: Example 3.5

--- general-linear-models/montgomery-peck-vining/chap03/examples/example-03-05/code/montgomery-example-03-05.R
+++ general-linear-models/montgomery-peck-vining/chap03/examples/example-03-05/code/montgomery-example-03-05.R
@@ -39,17 +39,48 @@ lm.full <- lm(
 	data    = DF.data
 	);
 
+lm.reduced <- lm(
+	formula = delivery.time ~ num.of.cases,
+	data    = DF.data
+	);
+
 ### VERIFICATION OF REGRESSION RESULTS #############################################################
-X.full <- cbind(rep(1,nrow(DF.data)),DF.data[,'num.of.cases'],DF.data[,'distance']);
+X.full    <- cbind(rep(1,nrow(DF.data)),DF.data[,'num.of.cases'],DF.data[,'distance']);
+X.reduced <- cbind(rep(1,nrow(DF.data)),DF.data[,'num.of.cases']);
 
 beta.full    <- solve(t(X.full) %*% X.full) %*% t(X.full) %*% DF.data[,'delivery.time'];
 yhat.full    <- X.full %*% beta.full;
-MS.full      <- sum((DF.data[,'delivery.time']-yhat.full)^2) / (nrow(DF.data) - ncol(X.full));
+df.full      <- nrow(DF.data) - ncol(X.full);
+MS.full      <- sum((DF.data[,'delivery.time']-yhat.full)^2) / df.full;
 beta.sd.full <- sqrt(diag(MS.full * solve(t(X.full) %*% X.full)));
 t.stat.full  <- beta.full / beta.sd.full;
-pvalue.full  <- my.pvalue(pt(q = t.stat.full, df = nrow(X.full) - ncol(X.full)));
+pvalue.full  <- my.pvalue(pt(q = t.stat.full, df = df.full));
+
+beta.reduced <- solve(t(X.reduced) %*% X.reduced) %*% t(X.reduced) %*% DF.data[,'delivery.time'];
+yhat.reduced <- X.reduced %*% beta.reduced;
+
 cbind(beta.full,beta.sd.full,t.stat.full,pvalue.full);
+ss01    <- sum(yhat.full^2) - sum(yhat.reduced^2);
+df01    <- ncol(X.full) - ncol(X.reduced);
+ms01    <- ss01 / df01;
+fstat01 <- ms01 / MS.full;
+pv01    <- pf(q = fstat01, df1 = df01, df2 = nrow(X.full) - ncol(X.full), lower.tail = FALSE);
+c(ss01,df01,ms01,fstat01,pv01);
+
+### ~~~~~~~~~~~~~~~~~~~~~~~~~~~
+### using R build-in functions:
 summary(lm.full);
+anova(lm.reduced,lm.full);
+
+### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+### comparing t-stat and F-stat:
+beta.full[3];
+beta.full[3]^2;
+beta.full[3]^2 / solve(t(X.full) %*% X.full)[3,3];
+
+# numerator of F-statistic (degree of freedom = 1)
+ss01 <- sum(yhat.full^2) - sum(yhat.reduced^2);
+ss01;
 
 q();
 
