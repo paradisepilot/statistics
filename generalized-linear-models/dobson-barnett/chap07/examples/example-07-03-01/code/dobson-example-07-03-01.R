62c4edbfc0d7ebd023d9daf9084c194e8af1c630
paradisepilot <paradisepilot@gmail.com>
2014-01-04 14:29:20 -0500

    added: Example 7.3.1

---
+++ generalized-linear-models/dobson-barnett/chap07/examples/example-07-03-01/code/dobson-example-07-03-01.R
@@ -0,0 +1,173 @@
+
+command.arguments <- commandArgs(trailingOnly = TRUE);
+data.directory   <- command.arguments[1];
+output.directory <- command.arguments[2];
+code.directory   <- command.arguments[3];
+tmp.directory    <- command.arguments[4];
+
+####################################################################################################
+library(MPV);
+library(ggplot2);
+
+#source(paste(code.directory, "bugs-chains.R", sep = "/"));
+#source(paste(code.directory, "which-bugs.R",  sep = "/"));
+
+####################################################################################################
+setwd(output.directory);
+
+####################################################################################################
+DF.data <- read.delim(
+	file = paste0(data.directory,'/table-07-02-beetle-mortality.csv'),
+	sep  = '\t'
+	);
+DF.data[,'num.alive'] <- DF.data[,'num.of.beetles'] - DF.data[,'num.killed'];
+DF.data[,'saturated.model'] <- factor(1:nrow(DF.data));
+str(DF.data);
+DF.data;
+
+####################################################################################################
+glm.results <- glm(
+	formula = cbind(num.killed,num.alive) ~ dose,
+	data    = DF.data,
+	family  = binomial(link = "logit")
+	);
+
+summary(glm.results);
+
+####################################################################################################
+### VERIFYING PARAMETER ESTIMATES AND FITTED VALUES
+
+coefficients(glm.results);
+
+X <- cbind(rep(1,nrow(DF.data)),DF.data[,'dose']);
+X;
+
+temp <- X %*% coefficients(glm.results);
+computed.predictors <- exp(temp) / (1 + exp(temp));
+
+predictors <- fitted.values(glm.results);
+cbind(
+	DF.data,
+	fitted = DF.data[,'num.of.beetles'] * predictors,
+	pi = predictors,
+	computed.pi = computed.predictors
+	);
+
+####################################################################################################
+summary(glm.results)[['cov.scaled']];
+summary(glm.results)[['cov.unscaled']];
+
+####################################################################################################
+### VERIFYING DEVIANCE FOR TWO-PARAMETER MODEL IN EXAMPLE 7.3.1
+
+# compute likelihood for saturated model
+saturated.glm.results <- glm(
+	formula = cbind(num.killed,num.alive) ~ saturated.model,
+	data    = DF.data,
+	family  = binomial(link = "logit")
+	);
+
+summary(saturated.glm.results);
+
+saturated.predictors <- fitted.values(saturated.glm.results);
+saturated.likelihood <- dbinom(
+	x    = DF.data[,'num.killed'],
+	size = DF.data[,'num.of.beetles'],
+	prob = saturated.predictors,
+	log  = FALSE
+	);
+
+prod(saturated.likelihood);
+log(prod(saturated.likelihood));
+
+# compute likelihood for two-parameter model in Example 7.3.1
+likelihood <- dbinom(
+	x    = DF.data[,'num.killed'],
+	size = DF.data[,'num.of.beetles'],
+	prob = predictors,
+	log  = FALSE
+	);
+likelihood;
+prod(likelihood);
+log(prod(likelihood));
+
+# compute deviance for two-parameter model in Exmaple 7.3.1:
+2 * ( log(prod(saturated.likelihood)) - log(prod(likelihood)) );
+deviance(glm.results);
+
+####################################################################################################
+### VERIYING DEVIANCE FOR NULL MODEL
+null.glm.results <- glm(
+	formula = cbind(num.killed,num.alive) ~ 1,
+	data    = DF.data,
+	family  = binomial(link = "logit")
+	);
+
+null.predictors <- fitted.values(null.glm.results);
+null.likelihood <- dbinom(
+	x    = DF.data[,'num.killed'],
+	size = DF.data[,'num.of.beetles'],
+	prob = null.predictors,
+	log  = FALSE
+	);
+prod(null.likelihood);
+log(prod(null.likelihood));
+
+# deviance for null model computed in three ways:
+2 * ( log(prod(saturated.likelihood)) - log(prod(null.likelihood)) );
+deviance(null.glm.results);
+glm.results[['null.deviance']];
+
+####################################################################################################
+
+q()
+
+### (a) ############################################################################################
+lm.results <- lm(
+	formula = y ~ x8,
+	data    = DF.data
+	);
+summary(lm.results);
+
+### (b) ############################################################################################
+SS.grand.mean <- sum((lm.results[['model']][,'y'] - mean(lm.results[['model']][,'y']))^2);
+SS.grand.mean;
+
+SS.full <- sum(residuals(lm.results)^2);
+SS.full;
+
+SS.grand.mean - SS.full;
+
+MS.full <- SS.full / (nrow(lm.results[['model']])-2);
+MS.full;
+
+F.statistic <- (SS.grand.mean - SS.full) / MS.full;
+F.statistic;
+
+pf(q = F.statistic, df1 = 1, df2 = nrow(lm.results[['model']])-2, lower.tail = FALSE);
+
+anova(lm.results);
+
+### (c) ############################################################################################
+X <- cbind(rep(1,nrow(DF.data)),DF.data[,'x8']);
+
+beta.hat <- solve( t(X) %*% X ) %*% t(X) %*% DF.data[,'y'];
+beta.hat;
+
+beta.Sigma <- MS.full * solve( t(X) %*% X );
+beta.Sigma;
+
+beta.hat.sd <- sqrt(beta.Sigma[2,2]);
+beta.hat.sd;
+
+beta.hat[2] + qt(p = 0.975, df = nrow(DF.data)-2) * beta.hat.sd * c(-1,1);
+
+### (d) ############################################################################################
+(SS.grand.mean - SS.full) / SS.grand.mean;
+
+### (e) ############################################################################################
+
+####################################################################################################
+
+q();
+
