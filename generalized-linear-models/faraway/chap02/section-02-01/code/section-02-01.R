9418393e43463a8e34b62b8b2c80258837878cc3
paradisepilot <paradisepilot@gmail.com>
2014-03-22 13:06:57 -0400

    updated: section-02-01.R

--- generalized-linear-models/faraway/chap02/section-02-01/code/section-02-01.R
+++ generalized-linear-models/faraway/chap02/section-02-01/code/section-02-01.R
@@ -21,11 +21,18 @@ orings;
 ####################################################################################################
 setwd(output.directory);
 
+
+results.lm <- lm(formula = damage/6 ~ temp, data = orings);
+slope      <- coef(results.lm)[['temp']];
+intercept  <- coef(results.lm)[['(Intercept)']];
+
 temp.filename <- 'figure-02-01.png';
 my.ggplot <- ggplot(data = NULL);
 my.ggplot <- my.ggplot + geom_point(mapping = aes(x = temp, y = damage/6), data = orings);
-my.ggplot <- my.ggplot + xlim(25,85);
-my.ggplot <- my.ggplot + ylim(0,1);
+my.ggplot <- my.ggplot + geom_abline(intercept = intercept, slope = slope);
+#my.ggplot <- my.ggplot + stat_smooth(method = "lm", se = FALSE, data = orings);
+my.ggplot <- my.ggplot + scale_x_continuous(limits = c(0,85), breaks = seq(0,80,10));
+my.ggplot <- my.ggplot + scale_y_continuous(limits = c(0,1),  breaks = seq(0,1,0.2));
 my.ggplot <- my.ggplot + xlab("temperature (Fahrenheit)");
 my.ggplot <- my.ggplot + theme(title = element_text(size = 20), axis.title = element_text(size = 30), axis.text  = element_text(size = 25));
 #my.ggplot <- my.ggplot + ggtitle(paste0("log.rank: ",formatC(logrank.stat),", pval: ",logrank.pval));
