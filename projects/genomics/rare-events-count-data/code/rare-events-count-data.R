68a621e7f5e71c0f966059ecca8ca156b9e0c82a
paradisepilot <paradisepilot@gmail.com>
2014-06-08 19:44:48 -0400

    updated: rare-events-count-data.R

--- projects/genomics/rare-events-count-data/code/rare-events-count-data.R
+++ projects/genomics/rare-events-count-data/code/rare-events-count-data.R
@@ -13,45 +13,101 @@ library(ggplot2);
 resolution <- 100;
 
 ####################################################################################################
+generate.DF.output <- function(
+	num.replicates = NULL,
+	lambdas        = NULL,
+	fold.change    = NULL
+	) {
+
+	DF.output <- data.frame(
+		lambda  = numeric(length(lambdas)),
+		log2.FC = numeric(length(lambdas)),
+		pval    = numeric(length(lambdas))
+		);
+
+	for (i in 1:length(lambdas)) {
+
+		temp.mean = lambdas[i];
+
+		lambda0 <- rnorm(n = num.replicates, mean = temp.mean, sd = temp.mean/100);
+		lambda1 <- rnorm(n = num.replicates, mean = fold.change * temp.mean, sd = fold.change * temp.mean/100);
+
+		obs0 <- rpois(n = num.replicates, lambda = lambda0);
+		obs1 <- rpois(n = num.replicates, lambda = lambda1);
+
+		DF.data <- rbind(
+			data.frame(count = obs0, group = rep('A',length(obs0))),
+			data.frame(count = obs1, group = rep('B',length(obs1)))
+			);
+
+		results.glm   <- glm(formula = count ~ group, data = DF.data, family = 'poisson');
+		results.anova <- anova(results.glm,test='LRT');
+
+		temp.pvalue   <- results.anova[['Pr(>Chi)']][2];
+		temp.log2.FC  <- log2(mean(obs1)/mean(obs0));
+		DF.output[i,] <- c(temp.mean,temp.log2.FC,temp.pvalue);
+
+		}
+
+	DF.output[,'minus.log10.pval'] <- -log10(DF.output[,'pval']);
+
+	return(DF.output);
+
+	}
+
+####################################################################################################
 setwd(output.directory);
 
-X  <- paste0('x',1:3);
-Y  <- paste0('y',1:2);
-XY <- expand.grid(X,Y);
-colnames(XY) <- c('X','Y');
+####################################################################################################
+num.replicates <- 5;
+lambdas        <- seq(1000,2e6,1000);
+fold.change    <- 1;
 
-prob.independent <- c( 8,12,20,12,18,30);
-prob.perturb     <- c( 1, 1,-2, 2,-1,-1);
+DF.output <- generate.DF.output(
+	num.replicates = num.replicates,
+	lambdas        = lambdas,
+	fold.change    = fold.change
+	);
+DF.output.1 <- cbind(DF.output, group = factor(rep('FC1',nrow(DF.output)),levels = paste0('FC',1:4)));
 
-sizes       <- seq(100,500000,1000);
-DF.prob     <- cbind(XY, prob = prob.independent + 0.2*prob.perturb);
-DF.output.1 <- data.frame(size = numeric(length(sizes)), pval = numeric(length(sizes)));
-for (i in 1:length(sizes)) {
-	temp.size     <- sizes[i];
-	DF.temp       <- cbind(XY, Z = rmultinom(n = 1, size = temp.size, prob = DF.prob[,'prob']));
-	results.glm   <- glm(formula = Z ~ X + Y + X:Y, data = DF.temp, family = 'poisson');
-	results.anova <- anova(results.glm,test='LRT');
-	temp.pvalue   <- results.anova[['Pr(>Chi)']][4];
-	DF.output.1[i,] <- c(temp.size,temp.pvalue);
-	}
-DF.output.1[,'log10.pval'] <- log10(DF.output.1[,'pval']);
-DF.output.1 <- cbind(DF.output.1, group = factor(rep('A',nrow(DF.output.1)),levels = c('A','B')));
+####################################################################################################
+num.replicates <- 5;
+lambdas        <- seq(10,100,1);
+fold.change    <- 2;
 
-sizes       <- seq(10,1000,10);
-DF.prob     <- cbind(XY, prob = prob.independent + 5 * prob.perturb);
-DF.output.2 <- data.frame(size = numeric(length(sizes)), pval = numeric(length(sizes)));
-for (i in 1:length(sizes)) {
-	temp.size     <- sizes[i];
-	DF.temp       <- cbind(XY, Z = rmultinom(n = 1, size = temp.size, prob = DF.prob[,'prob']));
-	results.glm   <- glm(formula = Z ~ X + Y + X:Y, data = DF.temp, family = 'poisson');
-	results.anova <- anova(results.glm,test='LRT');
-	temp.pvalue   <- results.anova[['Pr(>Chi)']][4];
-	DF.output.2[i,] <- c(temp.size,temp.pvalue);
-	}
-DF.output.2[,'log10.pval'] <- log10(DF.output.2[,'pval']);
-DF.output.2 <- cbind(DF.output.2, group = factor(rep('B',nrow(DF.output.2)),levels = c('A','B')));
+DF.output <- generate.DF.output(
+	num.replicates = num.replicates,
+	lambdas        = lambdas,
+	fold.change    = fold.change
+	);
+DF.output.2 <- cbind(DF.output, group = factor(rep('FC2',nrow(DF.output)),levels = paste0('FC',1:4)));
 
-DF.output <- rbind(DF.output.1,DF.output.2);
+####################################################################################################
+num.replicates <- 5;
+lambdas        <- seq(10,100,1);
+fold.change    <- 3;
+
+DF.output <- generate.DF.output(
+	num.replicates = num.replicates,
+	lambdas        = lambdas,
+	fold.change    = fold.change
+	);
+DF.output.3 <- cbind(DF.output, group = factor(rep('FC3',nrow(DF.output)),levels = paste0('FC',1:4)));
+
+####################################################################################################
+num.replicates <- 5;
+lambdas        <- seq(10,100,1);
+fold.change    <- 4;
+
+DF.output <- generate.DF.output(
+	num.replicates = num.replicates,
+	lambdas        = lambdas,
+	fold.change    = fold.change
+	);
+DF.output.4 <- cbind(DF.output, group = factor(rep('FC4',nrow(DF.output)),levels = paste0('FC',1:4)));
+
+####################################################################################################
+DF.output <- rbind(DF.output.1,DF.output.2,DF.output.3,DF.output.4);
 write.table(
 	file      = 'rare-events-count-data.csv',
 	x         = DF.output,
@@ -60,11 +116,22 @@ write.table(
 	row.names = FALSE
 	);
 
+####################################################################################################
 temp.filename <- 'pvalues.png';
 my.ggplot <- ggplot(data = NULL);
-my.ggplot <- my.ggplot + geom_point(mapping = aes(x = size, y = log10.pval, colour = group), data = DF.output, alpha = 0.5);
-my.ggplot <- my.ggplot + xlab("size of first group");
-my.ggplot <- my.ggplot + ylab("log10(p-value)");
+my.ggplot <- my.ggplot + geom_point(mapping = aes(x = lambda, y = minus.log10.pval, colour = group), data = DF.output, alpha = 0.5);
+my.ggplot <- my.ggplot + xlab(expression(mu[0]));
+my.ggplot <- my.ggplot + ylab(expression('- log'[10]('p-value'))); #ylab("-log10(p-value)");
+my.ggplot <- my.ggplot + theme(title = element_text(size = 20), axis.title = element_text(size = 30), axis.text  = element_text(size = 25));
+ggsave(file = temp.filename, plot = my.ggplot, dpi = resolution, height = 6, width = 12, units = 'in');
+
+####################################################################################################
+temp.filename <- 'volcanoplot.png';
+my.ggplot <- ggplot(data = NULL);
+my.ggplot <- my.ggplot + geom_point(mapping = aes(x = log2.FC, y = minus.log10.pval, colour = group), data = DF.output, alpha = 0.5);
+my.ggplot <- my.ggplot + xlab(expression('log'[2]('FC')));         #xlab("log2(FC)");
+my.ggplot <- my.ggplot + ylab(expression('- log'[10]('p-value'))); #ylab("-log10(p-value)");
+my.ggplot <- my.ggplot + scale_x_continuous(limits = 3.0 * c(-1,1));
 my.ggplot <- my.ggplot + theme(title = element_text(size = 20), axis.title = element_text(size = 30), axis.text  = element_text(size = 25));
 ggsave(file = temp.filename, plot = my.ggplot, dpi = resolution, height = 6, width = 12, units = 'in');
 
