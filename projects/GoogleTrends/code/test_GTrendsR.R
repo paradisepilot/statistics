c1ed5e13534c892bd6867fc4f04c82b70047d6b3
paradisepilot <paradisepilot@gmail.com>
2015-12-30 23:31:13 -0500

    updated things ...

--- projects/GoogleTrends/code/test_GTrendsR.R
+++ projects/GoogleTrends/code/test_GTrendsR.R
@@ -1,8 +1,5 @@
 
 ############################################################
-# library(devtools);
-# devtools::install_github("dvanclev/GTrendsR");
-
 command.arguments <- commandArgs(trailingOnly = TRUE);
 code.directory    <- normalizePath(command.arguments[1]);
 output.directory  <- normalizePath(command.arguments[2]);
@@ -13,23 +10,48 @@ setwd(output.directory);
 
 ############################################################
 library(yaml);
-library(GTrendsR);
+library(gtrendsR);
+library(ggplot2);
+library(reshape2);
 
 gCredentials <- yaml.load_file(input = '~/googleCredentials.yml');
 
+############################################################
 myCurlHandle <- gconnect(
 	usr = gCredentials[['username']],
 	psw = gCredentials[['password']]
 	);
 
+data("countries");
 gtrends.results <- gtrends(
 	ch    = myCurlHandle,
-	query = c('free shipping'),
-	geo   = "all",
-	cat   = "0"
+	query = c('free shipping','christmas tree')
+	);
+str(gtrends.results);
+
+############################################################
+DF.temp <- melt(
+	data         = gtrends.results[['trend']][,c('start','free.shipping','christmas.tree')],
+	id.vars      = c('start'), 
+	measure.vars = c('free.shipping','christmas.tree')
+	);
+summary(DF.temp);
+
+print(DF.temp);
+
+############################################################
+my.ggplot <- ggplot(data = NULL);
+
+my.ggplot <- my.ggplot + geom_line(
+	mapping = aes(x = start, y = value, colour = variable),
+	data    = DF.temp
 	);
 
-str(gtrends.results);
+temp.filename <- 'myGoogleTrends.png';
+ggsave(file = temp.filename, plot = my.ggplot, dpi = 150, height = 6, width = 12, units = 'in');
+
+############################################################
+q();
 
 png(file = "myplot.png", width = 2 * 480, height = 480);
 plot(
