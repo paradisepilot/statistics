18c502a699ab82097375c4cb1f69f3a2a908012e
paradisepilot <paradisepilot@gmail.com>
2016-01-16 00:26:35 -0500

    updated things ...

--- projects/DataForGood/2016-Youville/code/denormalizeData.R
+++ projects/DataForGood/2016-Youville/code/denormalizeData.R
@@ -8,9 +8,15 @@ denormalizeData <- function(
 	### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ###
 	depositItems <- read.csv(file=paste0(table.directory,"/DepositItems.txt"),sep='|');
 
+	### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ###
 	contacts     <- read.csv(file=paste0(table.directory,"/Contacts.txt"),    sep='|');
 	contacts[['PostalCode']] <- as.character(contacts[['PostalCode']]);
+	postal.codes <- unique(contacts[['PostalCode']]);
+	print('postal.codes');
+	print( postal.codes );
 
+
+	### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ###
 	temp <- left_join(
 		x  = depositItems,
 		y  = contacts,
