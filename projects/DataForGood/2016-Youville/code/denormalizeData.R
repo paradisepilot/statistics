6b234fd1a6f48e763154403c18d13319b41e6909
paradisepilot <paradisepilot@gmail.com>
2016-01-17 03:22:06 -0500

    updated things ...

--- projects/DataForGood/2016-Youville/code/denormalizeData.R
+++ projects/DataForGood/2016-Youville/code/denormalizeData.R
@@ -11,18 +11,42 @@ denormalizeData <- function(
 		table.directory = table.directory
 		);
 
+	deposits <- get.deposits(
+		table.directory = table.directory
+		);
+
+	print('summary(deposits)');
+	print( summary(deposits) );
+
 	### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ###
-	depositItems <- read.csv(file=paste0(table.directory,"/DepositItems.txt"),sep='|');
+	denormalized.depositItems <- read.csv(
+		file = paste0(table.directory,"/DepositItems.txt"),
+		sep  = '|'
+		);
+
+	denormalized.depositItems[['estate_donation']] <- as.logical(
+		denormalized.depositItems[['estate_donation']]
+		);
 
 	denormalized.depositItems <- left_join(
-		x  = depositItems,
+		x  = denormalized.depositItems,
 		y  = denormalized.contacts,
 		by = "ContactID"
+		);
+
+	print('setdiff(deposits$DepositNum,denormalized.depositItems$DepositNum)');
+	print( setdiff(deposits$DepositNum,denormalized.depositItems$DepositNum) );
+
+	denormalized.depositItems <- left_join(
+		x  = denormalized.depositItems,
+		y  = deposits,
+		by = "DepositNum"
 		);
 
 	### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ###
 	LIST.output <- list(
 		depositItems = denormalized.depositItems,
+		deposits     = deposits,
 		contacts     = denormalized.contacts
 		);
 
@@ -32,6 +56,28 @@ denormalizeData <- function(
 	}
 
 ##################################################
+get.deposits <- function(table.directory = NULL) {
+
+	deposits <- read.table(
+		file             = paste0(table.directory,"/Deposits.txt"),
+		quote            = "",
+		comment.char     = "",
+		header           = TRUE,
+		sep              = '|',
+		stringsAsFactors = FALSE
+		);
+
+	deposits <- deposits[,setdiff(colnames(deposits),'DepositNotes')];
+	deposits[['DepositDate']] <- cleanDates(
+		dates = as.character(deposits[['DepositDate']])
+		);
+	deposits[['Historical']] <- as.logical(deposits[['Historical']]);
+	deposits[['DepositRptPrinted']] <- as.logical(deposits[['DepositRptPrinted']]);
+
+	return(deposits);
+
+	}
+
 get.denormalized.contacts <- function(table.directory = NULL) {
 
 	### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ###
@@ -54,7 +100,7 @@ get.denormalized.contacts <- function(table.directory = NULL) {
 	### get (longitude,latitude) for postal/zip codes
 	postal.codes <- unique(contacts[['PostalCode']]);
 	DF.geocodes <- get.geocodes(
-		locations     = postal.codes[1101:1200],
+		locations     = postal.codes[1:1100],
 		tmp.directory = tmp.directory
 		);
 
@@ -103,9 +149,6 @@ get.denormalized.contacts <- function(table.directory = NULL) {
 		y  = provinces,
 		by = "ProvID"
 		);
-
-	print('str(denormalized.contacts)');
-	print( str(denormalized.contacts) );
 
 	### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ###
 	return(denormalized.contacts);
