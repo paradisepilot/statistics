ddc9de92278964f48bd0b11f3ca017e6a8803b89
paradisepilot <paradisepilot@gmail.com>
2016-01-19 21:31:22 -0500

    updated things ...

--- projects/DataForGood/2016-Youville/code/test_denormalizeData.R
+++ projects/DataForGood/2016-Youville/code/test_denormalizeData.R
@@ -19,19 +19,17 @@ source(paste0(code.directory,'/getYouvilleData.R'));
 setwd(output.directory);
 
 ###################################################
-
 #ggmap.ottawa <- get_map("ottawa", zoom = 12, source = "google", maptype = "roadmap");
-#str(ggmap.ottawa);
 
-#my.ggmap <- ggmap(ggmap = ggmap.ottawa, extent="device");
+#ggmap.ottawa <- get_map("ottawa", zoom = 15);
+#save(list = c("ggmap.ottawa"), file = "ggmap-ottawa-zoom15.RData");
 
-#resolution <- 600;
-#temp.filename <- 'ottawa-google-roodmap.png';
-#ggsave(file = temp.filename, plot = my.ggmap, dpi = resolution, height = 8, width = 8, units = 'in');
+#load("ggmap-ottawa.RData");
+#load("ggmap-ottawa-zoom10.RData");
+#load("ggmap-ottawa-zoom14.RData");
+load("ggmap-ottawa-zoom15.RData");
 
-### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ###
-
-#q();
+###################################################
 
 ### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ###
 FILE.geocodes <- paste0(table.directory,'/geocodes.txt');
@@ -76,6 +74,64 @@ write.table(
 	quote     = TRUE,
 	row.names = FALSE
 	);
+
+### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ###
+str(denormalized.donationReceipts);
+
+### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ###
+DF.temp <- denormalized.donationReceipts[['denormalized.donationReceipts']];
+
+summary(
+	DF.temp[,c('DonationReceiptDate','DonationReceiptDonationAmount')]
+	);
+
+#DF.temp[,'log10_DonationReceiptDonationAmount'] <- log10(DF.temp[,'DonationReceiptDonationAmount']);
+
+my.ggplot <- ggplot(data = NULL);
+my.ggplot <- my.ggplot + geom_point(
+	data    = DF.temp,
+	mapping = aes(x = DonationReceiptDate, y = log10(DonationReceiptDonationAmount))
+	);
+my.ggplot <- my.ggplot + scale_x_date(
+	date_labels        = "%b %Y",
+	limits             = c(as.Date("1996-1-1"),as.Date("2015-12-31")),
+	date_breaks        = "1 year"
+	#date_minor_breaks  = "6 months"
+	);
+my.ggplot <- my.ggplot + scale_y_continuous(limits=c(0,5.5));
+
+resolution <- 300;
+temp.filename <- 'Youville-donation-amount-by-time.png';
+ggsave(file = temp.filename, plot = my.ggplot, dpi = resolution, height = 8, width = 16, units = 'in');
+
+q();
+
+### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ###
+my.ggmap <- ggmap(ggmap = ggmap.ottawa, extent="device");
+my.ggmap <- my.ggmap + geom_point(
+	data   = denormalized.donationReceipts[['denormalized.donationReceipts']],
+	colour = "red",
+	alpha  = 0.3,
+	mapping = aes(
+		x    = DonationReceiptLongitude,
+		y    = DonationReceiptLatitude,
+		size = DonationReceiptDonationAmount
+		)
+	);
+
+resolution <- 600;
+temp.filename <- 'ottawa-map.png';
+ggsave(file = temp.filename, plot = my.ggmap, dpi = resolution, height = 8, width = 12, units = 'in');
+
+q();
+
+#str(ggmap.ottawa);
+
+#my.ggmap <- ggmap(ggmap = ggmap.ottawa, extent="device");
+
+#resolution <- 600;
+#temp.filename <- 'ottawa-google-roodmap.png';
+#ggsave(file = temp.filename, plot = my.ggmap, dpi = resolution, height = 8, width = 8, units = 'in');
 
 ###################################################
 
