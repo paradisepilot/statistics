21418cfc6b5a931053921830017b38efbcb6a543
paradisepilot <paradisepilot@gmail.com>
2016-10-29 16:18:30 -0400

    added statistics/projects/StatCan

---
+++ projects/StatCan/recordLinkage/errorAdjustment/chipperfield/001-StatCan-linkAdjust/StatCan.linkAdjust/R/make-synthetic-data.R
@@ -0,0 +1,5 @@
+
+make.synthetic.data <- function() {
+	return("Hello World!");
+	}
+
---
+++ projects/StatCan/recordLinkage/errorAdjustment/chipperfield/002-makeSyntheticData/execution/RUN_test-makeSyntheticData.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+currentDIR=`pwd`
+parentDIR="$(dirname "$currentDIR")"
+  codeDIR=${parentDIR}/code
+outputDIR=${parentDIR//github/gittmp}/output
+
+if [ ! -d ${outputDIR} ]; then
+	mkdir -p ${outputDIR}
+fi
+
+packageDIR=${parentDIR}
+packageDIR="$(dirname "$packageDIR")"
+packageDIR=${packageDIR}/001-StatCan-linkAdjust/StatCan.linkAdjust/R
+
+##################################################
+myRscript=${codeDIR}/test_makeSyntheticData.R
+stdoutFile=stdout.R.`basename ${myRscript} .R`
+R --no-save --args ${codeDIR} ${packageDIR} ${outputDIR} < ${myRscript} 2>&1 > ${stdoutFile}
+
+##################################################
+exit
+
