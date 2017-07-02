c10283d77c3fe48bd1de3d10f7dc4222a7d52bfc
paradisepilot <paradisepilot@gmail.com>
2017-07-01 20:12:48 -0400

    updated things ...

--- projects/SoftwareCarpentry/camera_analysis/execution/RUN_runall.sh
+++ projects/SoftwareCarpentry/camera_analysis/execution/RUN_runall.sh
@@ -6,10 +6,6 @@ parentDIR="$(dirname "$currentDIR")"
    datDIR=${parentDIR//github/gitdat}
    outDIR=${parentDIR//github/gittmp}/output
 
-echo ${srcDIR}
-echo ${datDIR}
-echo ${outDIR}
-
 if [ ! -d ${outDIR} ]; then
 	mkdir -p ${outDIR}
 fi
@@ -21,9 +17,6 @@ myPyScript=${srcDIR}/runall.py
 stdoutFile=stdout.py.`basename ${myPyScript} .py`
 stderrFile=stderr.py.`basename ${myPyScript} .py`
 python3 ${myPyScript} ${srcDIR} ${datDIR} ${outDIR} > ${outDIR}/${stdoutFile} 2> ${outDIR}/${stderrFile}
-
-# python3 ${myPyScript} ${srcDIR} ${outDIR} 2>&1 > ${stdoutFile}
-# python3 ../src/runall.py > stdout.runall-py 2> stderr.runall-py
 
 ##################################################
 exit
