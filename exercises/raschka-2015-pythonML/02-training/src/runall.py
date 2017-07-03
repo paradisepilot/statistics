b12bb844992fbd5514dad4816c26a8c0b69bf656
paradisepilot <paradisepilot@gmail.com>
2017-07-02 20:53:34 -0400

    updated things ...

--- exercises/raschka-2015-pythonML/02-training/src/runall.py
+++ exercises/raschka-2015-pythonML/02-training/src/runall.py
@@ -7,18 +7,6 @@ srcDIR     = sys.argv[1]
 datDIR     = sys.argv[2]
 outDIR     = sys.argv[3]
 
-print('thisScript')
-print( thisScript )
-
-print('srcDIR')
-print( srcDIR )
-
-print('datDIR')
-print( datDIR )
-
-print('outDIR')
-print( outDIR )
-
 # append module path with srcDIR
 sys.path.append(srcDIR)
 
@@ -28,17 +16,12 @@ sys.path.append(srcDIR)
 # change current working directory to outDIR
 #os.chdir(outDIR)
 
-### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ###
-# sys.exit(0)
-
-'''
-Script to create all results for camera_analysis project.
-'''
-
+#################################################
+#################################################
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-import Perceptron
+from   Perceptron import Perceptron
 
 # ------------------------------------------------------------------------
 # Declare variables
@@ -46,14 +29,46 @@ import Perceptron
 irisFILE = os.path.join(datDIR,'iris.data')
 irisDF   = pd.read_csv(irisFILE, header=None);
 
-print('irisDF.tail()')
-print( irisDF.tail() )
+irisDF.columns = [
+    'sepal_length',
+    'sepal_width',
+    'petal_length',
+    'petal_width',
+    'class' 
+    ]
+
+irisDF["class"] = irisDF["class"].astype('category')
+
+print('\nirisDF.tail()')
+print(   irisDF.tail() )
+
+print('\nirisDF.describe()')
+print(   irisDF.describe() )
+
+print('\nirisDF["class"].value_counts()')
+print(   irisDF["class"].value_counts() )
 
 ### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ###
+X = irisDF.iloc[0:100,[0,2]].values
+y = irisDF.iloc[0:100,    4].values
+y = np.where(y == 'Iris-setosa',-1,1);
+
 ### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ###
+figFILE = os.path.join(outDIR,'scatter.png')
+fig, ax = plt.subplots(1, 1)
+ax.scatter(X[   :50,0], X[   :50,1], color = 'red',  marker = 'o', label = 'setosa'    )
+ax.scatter(X[50:100,0], X[50:100,1], color = 'blue', marker = 'x', label = 'versicolor')
+fig.savefig(figFILE)
+
+### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ###
+ppn = Perceptron(eta = 0.1, n_iter = 10)
+#ppn.fit(X,y)
+
+#################################################
+#################################################
 sys.exit(0)
-### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ###
-### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ###
+#################################################
+#################################################
 
 #import numpy as np
 #import pandas as pd
