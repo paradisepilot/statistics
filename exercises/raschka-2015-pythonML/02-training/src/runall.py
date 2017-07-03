052a579a0a17c9c6f05b5efb1af070a5a6ea4094
paradisepilot <paradisepilot@gmail.com>
2017-07-03 13:39:46 -0400

    updated things ...

--- exercises/raschka-2015-pythonML/02-training/src/runall.py
+++ exercises/raschka-2015-pythonML/02-training/src/runall.py
@@ -16,6 +16,7 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 from   Perceptron import Perceptron
+from   AdalineGD  import AdalineGD 
 from   plotFunctions import plotDecisionRegions
 
 # ------------------------------------------------------------------------
@@ -49,7 +50,7 @@ y = irisDF.iloc[0:100,    4].values
 y = np.where(y == 'Iris-setosa',-1,1);
 
 ### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ###
-figFILE = os.path.join(outDIR,'scatter.png')
+figFILE = os.path.join(outDIR,'iris-scatter.png')
 fig, ax = plt.subplots(1, 1)
 ax.scatter(X[   :50,0], X[   :50,1], color = 'red',  marker = 'o', label = 'setosa'    )
 ax.scatter(X[50:100,0], X[50:100,1], color = 'blue', marker = 'x', label = 'versicolor')
@@ -59,8 +60,7 @@ fig.savefig(figFILE)
 ppn = Perceptron(eta = 0.1, n_iter = 10)
 ppn.fit(X,y)
 
-### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ###
-figFILE = os.path.join(outDIR,'error-epoch.png')
+figFILE = os.path.join(outDIR,'perceptron-error-epoch.png')
 fig, ax = plt.subplots(1, 1)
 ax.plot(
     range(1,len(ppn.errors_)+1),
@@ -71,7 +71,6 @@ ax.set_xlabel('epoch')
 ax.set_ylabel('number of misclassifications')
 fig.savefig(figFILE)
 
-### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ###
 figFILE = os.path.join(outDIR,'decision-regions.png')
 plotDecisionRegions(
     X          = X,
@@ -81,6 +80,32 @@ plotDecisionRegions(
     xlabel     = 'sepal length (cm)',
     ylabel     = 'petal length (cm)'
     )
+
+### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ###
+fig, ax = plt.subplots(nrows = 1, ncols = 2, figsize = (8,4))
+
+ada0 = AdalineGD(eta = 0.01, n_iter = 10).fit(X,y)
+ax[0].plot(
+    range(1,len(ada0.cost_)+1),
+    np.log10(ada0.cost_),
+    marker = 'o'
+    )
+ax[0].set_xlabel('epoch')
+ax[0].set_ylabel('log(sum of squared errors)')
+ax[0].set_title('Adaline - Learning rate 0.01')
+
+ada1 = AdalineGD(eta = 0.0001, n_iter = 10).fit(X,y)
+ax[1].plot(
+    range(1,len(ada1.cost_)+1),
+    ada1.cost_,
+    marker = 'o'
+    )
+ax[1].set_xlabel('epoch')
+ax[1].set_ylabel('sum of squared errors')
+ax[1].set_title('Adaline - Learning rate 0.0001')
+
+figFILE = os.path.join(outDIR,'adaline-error-epoch.png')
+fig.savefig(figFILE)
 
 #################################################
 #################################################
