bac78c6ecd70b016f91ef36ebf00e327fe08f792
paradisepilot <paradisepilot@gmail.com>
2017-08-06 17:59:14 -0400

    updated things ...

--- projects/StatCan/californiaR/code/train-predict-evaluate.R
+++ projects/StatCan/californiaR/code/train-predict-evaluate.R
@@ -39,20 +39,16 @@ my.predict <- function(DF.input, attributeAdder, trainedImputer, trainedModel) {
 
     require(caret);
 
-    ### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ###
     DF.imputed <- predict(object = trainedImputer, newdata = DF.input);
 
-    ### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ###
     DF.temp <- attributeAdder(DF.input = DF.imputed);
     DF.temp[,"median_house_value"] <- DF.input[,"median_house_value"];
 
-    ### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ###
     results.predict <- predict(
         object  = trainedModel, 
         newdata = DF.temp[,base::setdiff(colnames(DF.temp),"median_house_value")]
         );
 
-    ### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ###
     return( results.predict );
 
     }
