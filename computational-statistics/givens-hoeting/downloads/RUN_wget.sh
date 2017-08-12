59e893a04d258fbe5c40ca3a37fc0309284809ef
paradisepilot <paradisepilot@gmail.com>
2017-08-12 15:42:34 -0400

    removed computational-statistics/givens-hoeting

--- computational-statistics/givens-hoeting/downloads/RUN_wget.sh
+++
@@ -1,7 +0,0 @@
-
-# wget http://www.stat.colostate.edu/computationalstatistics/datasets/computationalstatistics.zip
-# unzip computationalstatistics.zip
-
-wget http://www.stat.colostate.edu/computationalstatistics/CompStatCode.zip
-unzip CompStatCode.zip
-
