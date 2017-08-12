3135b19df27bb406ab87ea9a060b2b63eb2ffd20
paradisepilot <paradisepilot@gmail.com>
2017-08-12 15:46:40 -0400

    removed design-of-experiments/

--- design-of-experiments/dean-voss/downloads/data/RUN_wget.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-#wget -r -l1 -A.txt http://www.wright.edu/~dvoss/bookdata/txt/aerosol.txt
-
-wget http://www.wright.edu/~dvoss/bookdata/txt/aerosol.txt
-wget http://www.wright.edu/~dvoss/bookdata/txt/heartlung.pump.txt
-
