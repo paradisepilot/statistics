f409e7f4eb60e3f41c9880f9aa5bf4d1130cfce6
paradisepilot <paradisepilot@gmail.com>
2017-09-16 13:33:33 -0400

    updated things ...

--- exercises/geron-handson-ML/03-classification/src/getMNIST.py
+++ exercises/geron-handson-ML/03-classification/src/getMNIST.py
@@ -5,8 +5,10 @@ import numpy as np
 def getMNIST( mnistFILE ):
 
     if os.path.isfile(path = mnistFILE) == True :
+        print("loading data from drive ...")
         mnist = np.load(file = mnistFILE)
     else :
+        print("downloading data from Internet ...")
         from sklearn.datasets import fetch_mldata
         mnist = fetch_mldata('MNIST original')
         np.save(file = mnistFILE, arr = mnist)
