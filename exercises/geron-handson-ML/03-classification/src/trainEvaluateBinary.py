1c666c1619c9f27b9d65b0f640da8227561e9b2e
paradisepilot <paradisepilot@gmail.com>
2017-09-17 13:32:55 -0400

    updated things ...

--- exercises/geron-handson-ML/03-classification/src/trainEvaluateBinary.py
+++ exercises/geron-handson-ML/03-classification/src/trainEvaluateBinary.py
@@ -1,7 +1,7 @@
 
 import numpy as np
-from sklearn.metrics import mean_squared_error
-from sklearn.model_selection import cross_val_score
+from sklearn.metrics         import confusion_matrix
+from sklearn.model_selection import cross_val_score, cross_val_predict
 
 ###################################################
 def trainEvaluateBinary(trainData, testData, myModel, modelName):
@@ -12,8 +12,6 @@ def trainEvaluateBinary(trainData, testData, myModel, modelName):
 
     ### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ###
     trainData['isFive'] = (trainData.loc[:,'label'] == 5)
-
-    ### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ###
     myModel.fit(
         X = trainData.loc[:,featureColumns],
         y = trainData.loc[:,'isFive']
@@ -21,8 +19,7 @@ def trainEvaluateBinary(trainData, testData, myModel, modelName):
 
     ### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ###
     nFold = 10
-
-    cvAccuracy = cross_val_score(
+    myCVAccuracy = cross_val_score(
         estimator = myModel,
         X         = trainData.loc[:,featureColumns],
         y         = trainData.loc[:,"isFive"],
@@ -31,11 +28,27 @@ def trainEvaluateBinary(trainData, testData, myModel, modelName):
         )
 
     ### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ###
+    myPredictions = cross_val_predict(
+        estimator = myModel,
+        X         = trainData.loc[:,featureColumns],
+        y         = trainData.loc[:,"isFive"],
+        cv        = nFold
+        )
+
+    myConfusionMatrix = confusion_matrix(
+        y_true = trainData.loc[:,"isFive"],
+        y_pred = myPredictions
+        )
+
+    ### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ###
     print("\n### ~~~~~~~~~~~~~~~~~~~~ ###")
     print("###  " + modelName)
 
     print("\nCV Accuracy (" + str(nFold) + "-fold):")
-    print(cvAccuracy)
+    print(myCVAccuracy)
+
+    print("\nConfusion Matrix (" + str(nFold) + "-fold):")
+    print(myConfusionMatrix)
 
     ### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ###
     return( None )
