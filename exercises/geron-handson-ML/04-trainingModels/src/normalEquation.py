5889a83b75685837b89922c4eb8d13f8f9f8cdcf
paradisepilot <paradisepilot@gmail.com>
2017-10-08 16:15:19 -0400

    updated things ...

--- exercises/geron-handson-ML/04-trainingModels/src/normalEquation.py
+++ exercises/geron-handson-ML/04-trainingModels/src/normalEquation.py
@@ -1,6 +1,7 @@
 
 import numpy as np
 import matplotlib.pyplot as plt
+from sklearn.linear_model import LinearRegression
 
 def normalEquation(X,y):
 
@@ -8,18 +9,27 @@ def normalEquation(X,y):
     print("The Normal Equation")
 
     ### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ###
-    X1 = np.c_[np.ones((X.shape[0],1)),X]
-    theta_best = np.linalg.inv(X1.T.dot(X1)).dot(X1.T).dot(y)
-    print("theta_best:")
-    print( theta_best )
+    # X1 = np.c_[np.ones((X.shape[0],1)),X]
+    # theta_best = np.linalg.inv(X1.T.dot(X1)).dot(X1.T).dot(y)
+    # print("theta_best:")
+    # print( theta_best )
+
+    # b0 = theta_best[0][0]
+    # b1 = theta_best[1][0]
 
     ### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ###
-    b0 = theta_best[0][0]
-    b1 = theta_best[1][0]
+    myLinReg = LinearRegression()
+    myLinReg.fit(X,y)
+
+    b0 = myLinReg.intercept_
+    b1 = myLinReg.coef_
+
+    ### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ###
+    print("b0 = " + str(b0))
+    print("b1 = " + str(b1))
 
     ### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ###
     outputFILE = 'normal-equation.png'
-    # myPlot = plt.scatter(X,y)
     fig, ax = plt.subplots()
     ax.scatter(X,y)
     ax.plot(X, b0 + b1 * X, color='red')
