02a80f6c368fb4c386589fb1a90a8375b3e6df0a
paradisepilot <paradisepilot@gmail.com>
2017-10-28 13:07:18 -0400

    added 10-ANNs/

---
+++ exercises/geron-handson-ML/10-ANNs/src/getIrisData.py
@@ -0,0 +1,43 @@
+
+import os.path, pickle
+import numpy as np
+
+##############################
+def getIrisData( irisFILE ):
+
+    ### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ###
+    if os.path.isfile(path = irisFILE) == False :
+        print("downloading the Iris data set from Internet ...")
+        from sklearn.datasets import load_iris
+        with open(file = irisFILE, mode = 'wb') as myFile:
+            pickle.dump(obj = load_iris(), file = myFile)
+
+    ### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ###
+    with open(file = irisFILE, mode = 'rb') as myFile:
+        irisData = pickle.load(myFile)
+
+    ### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ###
+    return( irisData )
+
+##############################
+def examineIrisData( irisData ):
+
+    print("\n### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ###")
+    print( "examineIrisData()" )
+    print( "\ntype(irisData): " + str(type(irisData)) )
+    print( "\ndir(irisData): "  + str(dir(irisData )) )
+
+    print( "\nirisData.feature_names: " + str(irisData.feature_names) )
+    print( "type(irisData.data): "      + str(type(irisData.data   )) )
+    print( "irisData.data.shape: "      + str(irisData.data.shape   ) )
+
+    print( "\nirisData.target_names: " + str(irisData.target_names) )
+    print( "type(irisData.target): "   + str(type(irisData.target)) )
+    print( "irisData.target.shape: "   + str(irisData.target.shape) )
+
+    print( "\nirisData.DESCR:" )
+    print( str(irisData.DESCR) )
+
+    ### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ###
+    return( None )
+
---
+++ exercises/geron-handson-ML/10-ANNs/src/runall.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+
+import os, getpass, shutil, stat, sys, time, pprint, logging, datetime
+
+thisScript = sys.argv[0]
+srcDIR     = sys.argv[1]
+datDIR     = sys.argv[2]
+outDIR     = sys.argv[3]
+
+# create outDIR if not yet exists
+if not os.path.exists(outDIR):
+    os.makedirs(outDIR)
+
+# change directory to outDIR
+os.chdir(outDIR)
+
+# copy srcDIR to srcCOPY
+srcCOPY = os.path.join(outDIR,"src")
+shutil.copytree(src = srcDIR, dst = srcCOPY)
+
+# append module path with srcCOPY
+sys.path.append(srcCOPY)
+
+# print system time
+myTime = "system time: " + datetime.datetime.now().strftime("%c")
+print( "\n" + myTime + "\n" )
+print("####################")
+
+#################################################
+#################################################
+
+### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ###
+# loading iris data
+from getIrisData import getIrisData, examineIrisData
+irisData = getIrisData(
+    irisFILE = os.path.join(datDIR,"iris.pickle")
+    )
+
+examineIrisData(irisData = irisData)
+
+### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ###
+# The Perceptron
+from thePerceptron import thePerceptron
+thePerceptron(irisData = irisData)
+
+#################################################
+#################################################
+print("\n####################\n")
+myTime = "system time: " + datetime.datetime.now().strftime("%c")
+print( myTime + "\n" )
+
