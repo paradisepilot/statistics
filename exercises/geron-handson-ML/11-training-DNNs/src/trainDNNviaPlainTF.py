9bac90609ea61635f7c5e3f05bd4f64ae19d0285
paradisepilot <paradisepilot@gmail.com>
2017-11-12 15:30:09 -0500

    updated things ...

--- exercises/geron-handson-ML/11-training-DNNs/src/trainDNNviaPlainTF.py
+++ exercises/geron-handson-ML/11-training-DNNs/src/trainDNNviaPlainTF.py
@@ -6,7 +6,7 @@ import tensorflow as tf
 from sklearn.metrics           import accuracy_score
 from sklearn.preprocessing     import StandardScaler
 from tensorflow.contrib.learn  import SKCompat, DNNClassifier, infer_real_valued_columns_from_input
-from tensorflow.contrib.layers import fully_connected
+from tensorflow.contrib.layers import fully_connected, batch_norm
 
 ##################################################
 def tfGetMNIST( mnistFILE ):
@@ -71,16 +71,39 @@ def trainDNNviaPlainTF( mnistFILE, checkpointPATH, nHidden1, nHidden2, learningR
         #logits  = neuronLayer(hidden2,nOutputs,"outputs")
 
         He_initializer = tf.contrib.layers.variance_scaling_initializer()
+        is_training = tf.placeholder(tf.bool, shape=(), name='is_training')
+        bn_params = {
+            'is_training'         : is_training,
+            'decay'               : 0.99,
+            'updates_collections' : None
+            }
+
         hidden1 = fully_connected(
             inputs              = X,
             num_outputs         = nHidden1,
             activation_fn       = leakyReLU, # tf.nn.elu,
             weights_initializer = He_initializer,
+            normalizer_fn       = batch_norm,
+            normalizer_params   = bn_params,
             scope               = "hidden1"
             )
 
-        hidden2 = fully_connected(hidden1,nHidden2,scope="hidden2")
-        logits  = fully_connected(hidden2,nOutputs,scope="outputs",activation_fn=None)
+        hidden2 = fully_connected(
+            inputs            = hidden1,
+            num_outputs       = nHidden2,
+            normalizer_fn     = batch_norm,
+            normalizer_params = bn_params,
+            scope             = "hidden2"
+            )
+
+        logits = fully_connected(
+            inputs            = hidden2,
+            num_outputs       = nOutputs,
+            activation_fn     = None,
+            normalizer_fn     = batch_norm,
+            normalizer_params = bn_params,
+            scope             = "outputs"
+            )
 
     with tf.name_scope("loss"):
         xEntropy = tf.nn.sparse_softmax_cross_entropy_with_logits(labels=y,logits=logits)
@@ -130,9 +153,9 @@ def trainDNNviaPlainTF( mnistFILE, checkpointPATH, nHidden1, nHidden2, learningR
                 for iteration in range(mnistData.train.num_examples // batchSize):
                     X_batch, y_batch = mnistData.train.next_batch(batchSize)
                     y_batch = np.matmul(y_batch,np.arange(y_batch.shape[1])).astype('int')
-                    mySession.run(trainingOp,feed_dict={X:X_batch,y:y_batch})
-                accuracyTrain = accuracy.eval(feed_dict={X:X_batch,y:y_batch})
-                accuracyTest  = accuracy.eval(feed_dict={X:X_test, y:y_test })
+                    mySession.run(trainingOp, feed_dict={is_training:True, X:X_batch,y:y_batch})
+                accuracyTrain = accuracy.eval(feed_dict={is_training:True, X:X_batch,y:y_batch})
+                accuracyTest  = accuracy.eval(feed_dict={is_training:False,X:X_test, y:y_test })
                 print("epoch: ", epoch, ", accuracy(train): ", accuracyTrain, ", accuracy(test): ", accuracyTest)
             savePath = mySaver.save(mySession,checkpointPATH)
 
