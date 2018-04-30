28eef3581398c9fbb80dd346398662df8ce0950c
paradisepilot <paradisepilot@gmail.com>
2018-04-29 20:25:45 -0400

    added: R/stcutils.R, man/stcutils.Rd

---
+++ projects/StatCan/stcutils/R/stcutils.R
@@ -0,0 +1,7 @@
+#' stcutils: A package to facilitate adherence to Statistics Canada's R coding
+#' best practices, in particular, in relation to reproducibility and
+#' visualization styling.
+#'
+#' @docType package
+#' @name stcutils
+NULL
