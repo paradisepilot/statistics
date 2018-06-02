393330614bdbd486c3264398afb8a9fbaf252e46
paradisepilot <paradisepilot@gmail.com>
2018-06-02 11:38:00 -0400

    added: sarkar-text-analytics-python/

---
+++ exercises/sarkar-text-analytics-python/03-processing-understanding-text/src/TextTokenization.py
@@ -0,0 +1,13 @@
+
+'''
+import matplotlib.pyplot as plt
+from pandas.plotting import scatter_matrix
+'''
+
+def textTokenization():
+
+    ### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ###
+
+    ### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ###
+    return( None )
+
---
+++ exercises/sarkar-text-analytics-python/03-processing-understanding-text/src/runall.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+
+import os, stat, sys
+
+thisScript = sys.argv[0]
+srcDIR     = sys.argv[1]
+datDIR     = sys.argv[2]
+outDIR     = sys.argv[3]
+
+# append module path with srcDIR
+sys.path.append(srcDIR)
+
+# change directory to outDIR
+os.chdir(outDIR)
+
+#################################################
+#################################################
+import seaborn as sns
+
+### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ###
+from TextTokenization import textTokenization
+
+### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ###
+textTokenization()
+
+#################################################
+#################################################
+sys.exit(0)
+
