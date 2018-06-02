9dc8faff1d8177133563d89a56531e5d14cf6853
paradisepilot <paradisepilot@gmail.com>
2018-06-02 13:09:36 -0400

    updated things ...

--- exercises/sarkar-text-analytics-python/03-processing-understanding-text/src/TextTokenization.py
+++ exercises/sarkar-text-analytics-python/03-processing-understanding-text/src/TextTokenization.py
@@ -1,12 +1,24 @@
 
 '''
-import matplotlib.pyplot as plt
-from pandas.plotting import scatter_matrix
+dummy comment
 '''
 
-def textTokenization():
+import nltk
+from nltk.corpus import gutenberg
+from pprint      import pprint
+
+def sentenceTokenization():
 
     ### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ###
+    alice = gutenberg.raw(fileids = 'carroll-alice.txt')
+
+    sample_text = 'We will discuss briefly about the basic syntax, structure and design philosophies. There is a defined hierarchical syntax for Python code which you should remember when writing code! Python is a really powerful programming language!'
+
+    ### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ###
+    print( "\n### len(alice), number of characters: " + str(len(alice)) )
+
+    print( "\n### First 1000 characters of carroll-alice.txt:\n" )
+    print( alice[0:1000] )
 
     ### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ###
     return( None )
--- exercises/sarkar-text-analytics-python/03-processing-understanding-text/src/runall.py
+++ exercises/sarkar-text-analytics-python/03-processing-understanding-text/src/runall.py
@@ -7,6 +7,10 @@ srcDIR     = sys.argv[1]
 datDIR     = sys.argv[2]
 outDIR     = sys.argv[3]
 
+import datetime
+print( "\n\n#### system time: " + str(datetime.datetime.now()) )
+print( "\n###############################\n" )
+
 # append module path with srcDIR
 sys.path.append(srcDIR)
 
@@ -16,14 +20,23 @@ os.chdir(outDIR)
 #################################################
 #################################################
 import seaborn as sns
+import nltk
+
+from TextTokenization import sentenceTokenization
 
 ### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ###
-from TextTokenization import textTokenization
+# install gutenberg corpus if not yet installed
+try:
+    nltk.data.find('corpora/gutenberg')
+except LookupError:
+    nltk.download('gutenberg')
 
 ### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ###
-textTokenization()
+sentenceTokenization()
 
 #################################################
 #################################################
+print( "\n\n###############################" )
+print( "\n#### system time: " + str(datetime.datetime.now()) + "\n" )
 sys.exit(0)
 
