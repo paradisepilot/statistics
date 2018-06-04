dca5a09273796bbc770b5e76ec860bc47ef232f8
paradisepilot <paradisepilot@gmail.com>
2018-06-03 23:40:54 -0400

    updated things ...

--- exercises/sarkar-text-analytics-python/03-processing-understanding-text/src/CorrectRepeatCharacters.py
+++ exercises/sarkar-text-analytics-python/03-processing-understanding-text/src/CorrectRepeatCharacters.py
@@ -3,24 +3,34 @@
 dummy comment
 '''
 
-import nltk, re, string
-
-from nltk.corpus  import gutenberg, europarl_raw
-from pprint       import pprint
-from contractions import CONTRACTION_MAP
+import re
 
 ###################################################
 def correctRepeatCharacters():
 
     old_word = 'finalllyyy'
 
-    repeat_pattern     = re.compile(r"(\w*)(\w)\2(\w*)")
-    match_substitution = r"\1\2\3"
+    repeat_pattern = re.compile( r"(\w*)(\w)\2(\w*)" )
+    # Group 1: (\w*) = zero or more word character, followed by
+    # Group 2: (\w)  = single word character 
+    #          \2    = a repeated occurence of Group 2, in this
+    #                  case, the single word character matched
+    #                  in Group 2, followed by
+    # Group 3: (\w*) = zero or more word charater
+
+    replacement_pattern = r"\1\2\3"
+    # replacement pattern
+    # Technically: Group 1, followed by Group 2, followed by
+    # Group 3, where Groups 1,2,3 are the groups of
+    # characters matched above. Recall that Group 2 must
+    # appear two or more times. Thus, a replacement pattern
+    # of "\1\2\3" essentially removes one repeated occurence
+    # of Group 2.
 
     step = 1
     while True:
         # remove one repeated character
-        new_word = repeat_pattern.sub(match_substitution,old_word)
+        new_word = repeat_pattern.sub(repl = replacement_pattern, string = old_word)
         if new_word != old_word:
             print( 'Step: {}, Word: {}'.format(step,new_word) )
             step += 1 # update step
