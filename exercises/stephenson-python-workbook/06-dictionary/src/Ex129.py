2ca0cfd3fdd7b6066e4c2a55dc1d2f35b534ee5c
paradisepilot <paradisepilot@gmail.com>
2018-06-24 13:34:54 -0400

    updated things ...

--- exercises/stephenson-python-workbook/06-dictionary/src/Ex129.py
+++ exercises/stephenson-python-workbook/06-dictionary/src/Ex129.py
@@ -15,19 +15,41 @@ def ex129():
     print("\n### ~~~~~ Exercise 129 ~~~~~~~~");
 
     ### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ###
-    results = {}
+    results      = {}
+    expectations = {}
     for k in range(2,13):
-        results[k] = 0
+        results[k]      = 0
+        expectations[k] = 0
 
     ### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ###
-    nRolls = 1000
+    expectations[ 2] = 1/36
+    expectations[12] = 1/36
+
+    expectations[ 3] = 2/36
+    expectations[11] = 2/36
+
+    expectations[ 4] = 3/36
+    expectations[10] = 3/36
+
+    expectations[ 5] = 4/36
+    expectations[ 9] = 4/36
+
+    expectations[ 6] = 5/36
+    expectations[ 8] = 5/36
+
+    expectations[ 7] = 6/36
+
+    ### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ###
+    nRolls = 1000000
     for i in range(0,nRolls):
         temp_result = sumTwoDieRolls()
         results[temp_result] = results[temp_result] + 1
 
     ### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ###
+    print( "%6s %7s %8s %8s" % ('rolled','count','observed','expected') )
+    print( "%6s %7s %8s %8s" % ( 'total',''     ,' percent',' percent') )
     for k in results.keys():
-        print( str(k) + ": " + str(results[k]) + ", " + str(100*results[k]/nRolls) )
+        print( "%6d %7d %8.3f %8.3f" % (k,results[k],100*results[k]/nRolls,100*expectations[k]) )
 
     ### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ###
     return( None )
