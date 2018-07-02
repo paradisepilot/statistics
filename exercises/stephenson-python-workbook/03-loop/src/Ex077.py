e90c1173a73bc58dc89ca65286b166fb8530325f
paradisepilot <paradisepilot@gmail.com>
2018-07-02 15:39:32 -0400

    updated things ...

---
+++ exercises/stephenson-python-workbook/03-loop/src/Ex077.py
@@ -0,0 +1,39 @@
+
+'''
+dummy comment
+'''
+
+def binary2Decimal( x ):
+
+    reverselist_x = list(x)
+    reverselist_x.reverse()
+    reverselist_x = list(map(int,reverselist_x))
+
+    length_x = len(reverselist_x)
+
+    decimal = 0
+    for i in range(0,length_x):
+        decimal += reverselist_x[i] * (2**i)
+
+    output = { 'input': x, 'decimal':decimal }
+
+    return( output )
+
+def ex077():
+
+    print("\n### ~~~~~ Exercise 076 ~~~~~~~~");
+
+    ### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ###
+    myBinaryIntegers = [
+         '1100110',
+         '1101011',
+        '10001011'
+        ]
+
+    for myBinaryInteger in myBinaryIntegers:
+        results = binary2Decimal( x = myBinaryInteger )
+        print( results )
+
+    ### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ###
+    return( None )
+
