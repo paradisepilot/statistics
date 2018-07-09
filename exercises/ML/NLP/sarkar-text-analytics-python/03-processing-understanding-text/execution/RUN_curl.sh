d8e60bd965ddeec03aeef0a316d07897c745a8f7
paradisepilot <paradisepilot@gmail.com>
2018-07-09 07:22:58 -0400

    updated things ...

--- exercises/ML/NLP/sarkar-text-analytics-python/03-processing-understanding-text/execution/RUN_curl.sh
+++ exercises/ML/NLP/sarkar-text-analytics-python/03-processing-understanding-text/execution/RUN_curl.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
-curl -O https://raw.githubusercontent.com/ageron/handson-ml/master/datasets/housing/housing.tgz
-tar -xvzf housing.tgz
+
+curl -O http://norvig.com/big.txt
+
+#curl -O https://raw.githubusercontent.com/ageron/handson-ml/master/datasets/housing/housing.tgz
+#tar -xvzf housing.tgz
 
