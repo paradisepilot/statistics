29dde41ffe0c3bf8199169f4ca69cddea590c54e
paradisepilot <paradisepilot@gmail.com>
2018-07-09 07:52:35 -0400

    added: lane-NLP-in-action/

---
+++ exercises/ML/NLP/lane-NLP-in-action/02-vocabulary/execution/RUN_curl.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+curl -O http://norvig.com/big.txt
+
+#curl -O https://raw.githubusercontent.com/ageron/handson-ml/master/datasets/housing/housing.tgz
+#tar -xvzf housing.tgz
+
---
+++ exercises/ML/NLP/lane-NLP-in-action/02-vocabulary/src/runall.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+
+import os, stat, sys
+
+thisScript = sys.argv[0]
+srcDIR     = sys.argv[1]
+datDIR     = sys.argv[2]
+outDIR     = sys.argv[3]
+
+import datetime
+print( "\n\n#### system time: " + str(datetime.datetime.now()) )
+print( "\n###############################\n" )
+
+# append module path with srcDIR
+sys.path.append(srcDIR)
+
+# change directory to outDIR
+os.chdir(outDIR)
+
+#################################################
+#################################################
+import seaborn as sns
+
+#from InstallNLTKResources    import installNLTKResources
+#from TextTokenization        import sentenceTokenization, wordTokenization
+#from TextNormalization       import textNormalization
+#from CorrectRepeatCharacters import correctRepeatCharacters
+
+### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ###
+#installNLTKResources()
+
+### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ###
+### Text Tokenization
+# sentenceTokenization()
+# wordTokenization()
+
+### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ###
+### Text Normalization
+#textNormalization( datDIR = datDIR )
+
+### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ###
+### Correct Repeat Characters
+# correctRepeatCharacters()
+
+#################################################
+#################################################
+print( "\n\n###############################" )
+print( "\n#### system time: " + str(datetime.datetime.now()) + "\n" )
+sys.exit(0)
+
