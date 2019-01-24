2a7269b5d05400c621aebe141c295a3ebe67aa15
paradisepilot <paradisepilot@gmail.com>
2019-01-24 07:50:26 -0500

    updated things ...

--- exercises/ML/general/chollet-DL-python/03-getting-started-NN/src/section3pt4.py
+++ exercises/ML/general/chollet-DL-python/03-getting-started-NN/src/section3pt4.py
@@ -54,6 +54,34 @@ def section3pt4():
     print(   y_train.shape )
 
     ### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ###
+    ### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ###
+    model = models.Sequential()
+    model.add(layers.Dense(16, activation='relu', input_shape=(10000,)))
+    model.add(layers.Dense(16, activation='relu'))
+    model.add(layers.Dense( 1, activation='sigmoid'))
+
+    model.compile(
+        optimizer = 'rmsprop',
+        loss      = 'binary_crossentropy',
+        metrics   = ['accuracy']
+        )
+
+    model.fit(x_train, y_train, epochs = 4, batch_size = 512)
+
+    results = model.evaluate(x_test, y_test)
+    print('\nresults (4 epochs)')
+    print(   results )
+
+    predictions = model.predict(x_test)
+    print('\npredictions (4 epochs)')
+    print(   predictions )
+
+    ### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ###
+    ### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ###
+    # return( None )
+
+    ### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ###
+    ### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ###
     model = models.Sequential()
 
     model.add(layers.Dense(16, activation='relu', input_shape=(10000,)))
