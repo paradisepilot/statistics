90d63ca30972d89f8426ed4ce49c4677f55d0821
paradisepilot <paradisepilot@gmail.com>
2019-04-11 07:51:22 -0400

    updated things ...

--- projects/StatCan/opioid/01-test-tick/src/testHawkesEM.py
+++ projects/StatCan/opioid/01-test-tick/src/testHawkesEM.py
@@ -48,7 +48,7 @@ def test_HawkesEM():
 
     ### ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ ###
     realizations = list()
-    for i in range(0, 3):
+    for i in range(0,100):
 
         print( '' )
 
